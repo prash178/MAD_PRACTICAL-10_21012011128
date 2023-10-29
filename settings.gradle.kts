6017bc32adb3f8ff19779fdf38c3a85ec3abc6ce
prashant <rathodprashnat359@gmail.com>
2023-10-29 09:35:52 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_PRACTICAL-10_21012011128"
+include(":app")
+ 
